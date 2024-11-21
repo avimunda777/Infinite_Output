416f35723700379aa036ad2dfe974437944eb809
Avinash Singh Munda <96285074+avimunda777@users.noreply.github.com>
2024-11-22 02:30:40 +0530

    Add files via upload

---
+++ arr.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+    int i; // number of arguments to process each command line argument 
+    int j; // number of arguments to process each command line argument
+    printf("Processing %d arguments\n", i + 1);
+    printf("Processing %d arguments\n", j + 1);
+    for(i = 0; i < i + 1; i++)
+    for(j = 0; j < j + 1; j++) 
+    printf("%d" , i + j);
+    return 0; // return 0 if everything went well and everything failed otherwise return error code and exit code
+}
